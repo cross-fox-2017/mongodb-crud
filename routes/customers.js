0ce7626535ff19857836663fc9cc07f617bebdd6
yonisetiawan <yonisetiawan1990@gmail.com>
2017-01-31 17:13:25 +0700

    Customer Done

--- routes/customers.js
+++ routes/customers.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const customers = require('../controller/customer')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET users listing. */
+router.post('/', customers.insertData)
+router.get('/', customers.showData)
+router.get('/:id', customers.showDataById)
+router.get('/memberid/:memberid', customers.showDataByIsbn)
+router.put('/:memberid', customers.updateBook)
+router.delete('/:memberid',customers.deleteData)
+
 
 module.exports = router;
